f8f3793682b21bb30f90ba71f9b9715d3e7ec4ec
Arun-Prasanth-P <165559492+Arun-Prasanth-P@users.noreply.github.com>
2024-10-12 14:15:40 +0530

    Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-center text-lg-start bg-secondary-subtle fw-bold user-select-none">
-      <div className="text-center p-3">&copy; 2024 Copyright: RYD-Projects</div>
     </footer>
   );
 };
